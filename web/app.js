31ff5ee2ade82533f7063f3000ace117667fc5d6
Matthew Pace <pace4@purdue.edu>
2015-02-27 22:34:06 -0600

    added queue functionality

--- web/app.js
+++ web/app.js
@@ -1,6 +1,17 @@
 
-var app = angular.module('DJayyApp');
+var app = angular.module('DJayyApp', []);
 
-app.controller('DJayyCtrl', ['$scope', function($scope) {
+app.controller('MainCtrl', ['$scope', function($scope) {
+    
+    $scope.tracks = [];
 
+    $scope.addTrack = function(title, artist) {
+        var track = {
+            title: title,
+            artist: artist
+        };
+
+        $scope.tracks.push(track);
+        $scope.$apply();
+    }
 }]);

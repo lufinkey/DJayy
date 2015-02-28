35f0e6bb684370cb68ffb727e1b7133b65b37b36
lufinkey <luisfinke@gmail.com>
2015-02-28 01:33:56 -0500

    Added String and ArrayList. Setup correct server dir

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,11 +1,43 @@
 
 #include "DJayy/WebServer.h"
+#include "Util/String.h"
+
+#include <stdio.h>
+#ifdef _WIN32
+	#include <direct.h>
+#else
+	#include <unistd.h>
+#endif
 
 using namespace DJayy;
 
+String getCurrentWorkingDirectory()
+{
+	char buffer[FILENAME_MAX];
+#ifdef _WIN32
+	_getcwd(buffer, FILENAME_MAX);
+#else
+	getcwd(buffer, FILENAME_MAX);
+#endif
+	return buffer;
+}
+
+String getWebRoot()
+{
+	String workingDir = getCurrentWorkingDirectory();
+	char slash = '/';
+	size_t lastSlash = workingDir.lastIndexOf('/');
+	if(lastSlash == STRING_NOTFOUND)
+	{
+		lastSlash = workingDir.lastIndexOf('\\');
+		slash = '\\';
+	}
+	return workingDir.substring(0,lastSlash) + slash + "web";
+}
+
 int main(int argc, char *argv[])
 {
-	WebServer server("../web", 8080);
+	WebServer server(getWebRoot(), 8080);
 	server.start();
 
 	std::system("PAUSE");
